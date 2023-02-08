df3cc8a618d9904eabd9f9a2a42ff6234d122ecf
Frank Pérez <fp848584@gmail.com>
2023-02-08 01:15:36 -0500

    gallery section

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,6 +39,34 @@ export default function Home() {
       image:"/next.svg",
       description: "Esto es una hermosa descripcion de un producto que es incluso mas caro que el anterior",
       price: "2000"
+    },
+    {
+      id: 1,
+      name: "Name",
+      image:"/next.svg",
+      description: "Esto es una hermosa descripcion de este producto que es muy caro",
+      price: "100"
+    },
+    {
+      id: 2,
+      name: "Name2",
+      image:"/next.svg",
+      description: "Esto es una hermosa descripcion de un producto que es incluso mas caro que el anterior",
+      price: "2000"
+    },
+    {
+      id: 1,
+      name: "Name",
+      image:"/next.svg",
+      description: "Esto es una hermosa descripcion de este producto que es muy caro",
+      price: "100"
+    },
+    {
+      id: 2,
+      name: "Name2",
+      image:"/next.svg",
+      description: "Esto es una hermosa descripcion de un producto que es incluso mas caro que el anterior",
+      price: "2000"
     }
   ]
   const[route, setRoute] = useState("/");
